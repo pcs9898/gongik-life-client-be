10b1b7c3dac88d75c849434d8450ba9a389b48bf
Chansoo Park <pcs98pcs@gmail.com>
2025-01-04 18:50:28 +0900

    InitialSetting#4
    Create gongik-life-client-be-community-service

---
+++ gongik-life-client-be-community-service/src/main/java/org/example/gongiklifeclientbecommunityservice/GongikLifeClientBeCommunityServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.gongiklifeclientbecommunityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongikLifeClientBeCommunityServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GongikLifeClientBeCommunityServiceApplication.class, args);
+  }
+
+}
