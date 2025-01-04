d1bec81ff6c5958f45111e5e4a5696ce20a4cc23
Chansoo Park <pcs98pcs@gmail.com>
2025-01-04 18:37:57 +0900

    InitialSetting#3
    Create gongik-life-client-be-institution-service

---
+++ gongik-life-client-be-user-service/src/main/java/org/example/gongiklifeclientbeuserservice/GongikLifeClientBeUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.gongiklifeclientbeuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongikLifeClientBeUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongikLifeClientBeUserServiceApplication.class, args);
+	}
+
+}
---
+++ gongik-life-client-be-user-service/src/test/java/org/example/gongiklifeclientbeuserservice/GongikLifeClientBeUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gongiklifeclientbeuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongikLifeClientBeUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
