e90ffb7f0249cc64d7d00ef31c3160e737b1b9b5
Chansoo Park <pcs98pcs@gmail.com>
2025-01-04 17:00:50 +0900

    InitialSetting#1
    Create
    gongik-life-client-be-discovery
    gongik-life-client-be-grpc-common
    gongik-life-client-be-gateway
    gongik-life-client-be-auth-service

---
+++ gongik-life-client-be-auth-service/src/main/java/org/example/gongiklifeclientbeauthservice/GongikLifeClientBeAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.gongiklifeclientbeauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongikLifeClientBeAuthServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GongikLifeClientBeAuthServiceApplication.class, args);
+  }
+
+}
---
+++ gongik-life-client-be-gateway/src/test/java/org/example/gongiklifeclientbegateway/GongikLifeClientBeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gongiklifeclientbegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongikLifeClientBeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
