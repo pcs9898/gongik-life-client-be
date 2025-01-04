fe1d42ee3219a8fcaaaf8cbcd53571b94c13a0ff
Chansoo Park <pcs98pcs@gmail.com>
2025-01-04 19:10:44 +0900

    InitialSetting#5
    Create gongik-life-client-be-workhours-service

---
+++ gongik-life-client-be-workhours-service/src/main/java/org/example/gongiklifeclientbeworkhoursservice/GongikLifeClientBeWorkhoursServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.gongiklifeclientbeworkhoursservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongikLifeClientBeWorkhoursServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GongikLifeClientBeWorkhoursServiceApplication.class, args);
+  }
+
+}
