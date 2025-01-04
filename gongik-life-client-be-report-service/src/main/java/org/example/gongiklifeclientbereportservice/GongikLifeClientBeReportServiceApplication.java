15456d89d90693d4dd459ae7f92a93f376093fd0
Chansoo Park <pcs98pcs@gmail.com>
2025-01-04 20:34:28 +0900

    InitialSetting#6
    Create gongik-life-client-be-report-service

---
+++ gongik-life-client-be-report-service/src/main/java/org/example/gongiklifeclientbereportservice/GongikLifeClientBeReportServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.gongiklifeclientbereportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongikLifeClientBeReportServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GongikLifeClientBeReportServiceApplication.class, args);
+  }
+
+}
