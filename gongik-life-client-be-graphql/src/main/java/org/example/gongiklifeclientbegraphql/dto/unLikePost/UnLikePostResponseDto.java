bf2f8d08afdd95a49faa1cf8557e427cb03adebe
chan <pcs98pcs@gmail.com>
2025-02-16 04:42:45 +0900

    feature/25unLikePost
    Create unLike post api (graphql -> kafka)

---
+++ gongik-life-client-be-graphql/src/main/java/org/example/gongiklifeclientbegraphql/dto/unLikePost/UnLikePostResponseDto.java
@@ -0,0 +1,15 @@
+package org.example.gongiklifeclientbegraphql.dto.unLikePost;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnLikePostResponseDto {
+
+  private Boolean success;
+}
